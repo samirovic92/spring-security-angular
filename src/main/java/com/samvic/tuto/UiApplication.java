f48a61602586c95db76fa09141475ae7857b84c1
samir <s.elidrissi1992@gmail.com>
2022-10-02 11:36:28 +0200

    Refacto

--- src/main/java/com/samvic/tuto/UiApplication.java
+++ src/main/java/com/samvic/tuto/UiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSecurityAngularApplication {
+public class UiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringSecurityAngularApplication.class, args);
+        SpringApplication.run(UiApplication.class, args);
     }
 
 }
