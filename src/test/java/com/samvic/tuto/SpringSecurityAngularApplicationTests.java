b75f5cdef92688155944916614d672e8404c96b1
samir <s.elidrissi1992@gmail.com>
2022-10-02 11:15:28 +0200

    init project

---
+++ src/test/java/com/samvic/tuto/SpringSecurityAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samvic.tuto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAngularApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
